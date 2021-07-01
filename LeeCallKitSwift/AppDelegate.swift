cc0205bfb8df92b1053bce7ebe6c59e5bde892bd
leeyonghe <lyhdra99@gmail.com>
2021-07-01 23:35:29 +0900

    init

--- LeeCallKitSwift/AppDelegate.swift
+++ LeeCallKitSwift/AppDelegate.swift
@@ -7,10 +7,11 @@
 
 import UIKit
 import CallKit
+import PushKit
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+class AppDelegate: UIResponder, UIApplicationDelegate, PKPushRegistryDelegate {
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -28,6 +29,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        
+    }
+    
+    func voipRegistration() -> Void {
+        let voipRegistry = PKPushRegistry.init(queue: DispatchQueue.main)
+        voipRegistry.delegate = self
+        voipRegistry.desiredPushTypes = [PKPushType.voIP];
+        
+    }
+    
+    func pushRegistry(_ registry: PKPushRegistry, didUpdate pushCredentials: PKPushCredentials, for type: PKPushType) {
+        
     }
 
 
